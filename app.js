d7dd969ff82ec492c4214e6dfc3b997000e69d2c
lucia <luciareami11@gmail.com>
2021-06-14 21:13:07 -0300

    ultimos arreglos

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require ("path")
+const app = express ();
+const publicPath = path.resolve(__dirname, "./public") 
+app.use( express.static(publicPath))
+app.listen(9000, () => {
+    console.log("Servidor corriendo en el puerto 1000")
+});
+    app.get("/", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "./views/Home.Html")) 
+    })
+    app.get('/register', (req, res) => {
+        res.sendFile(path.resolve(__dirname, './views/register.html'))
+    });
+    
+    app.get('/login', (req, res) => {
+        res.sendFile(path.resolve(__dirname, './views/login.html'))
+    })
